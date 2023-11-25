8301762358c5a931747d7d03ed41a1c22b34a13c
gshiva <gshiva@hotmail.com>
2023-11-24 21:15:32 -0800

    Updated github_issue_updater.py to take in a csv file as input

--- docs/github-issue-updater/github_issue_updater.py
+++ docs/github-issue-updater/github_issue_updater.py
@@ -1,9 +1,12 @@
+
+import json
 import requests
+import pandas as pd
 
 # Set your parameters here
 GITHUB_TOKEN = 'token'
-REPO_OWNER = 'username or organization'
-REPO_NAME = 'repo name'
+REPO_OWNER = 'gshiva'
+REPO_NAME = 'pygptcourse'
 
 # Headers for authorization and to ensure the use of the API v3
 headers = {
@@ -15,13 +18,9 @@ def list_issues():
     """ List all issues from the repository """
     issues_url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/issues'
     response = requests.get(issues_url, headers=headers)
-    response = requests.get(issues_url, headers=headers)
     if response.status_code == 200:
         try:
             issues = response.json()
-            # Debug: print the type and content of the issues variable
-            print(f"Received data type: {type(issues)}")
-            print(f"Received data: {issues}")
             return issues
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
@@ -31,46 +30,36 @@ def list_issues():
         print(response.text)
         return []  # Return an empty list on error
 
-    return response.json()
-
-def update_issue(issue_number, new_title, new_body, labels, milestone):
-    """ Update the issue with a new title and body """
+def update_issue(issue_number, new_body):
+    """ Update the issue with a new body """
     issue_url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/issues/{issue_number}'
     data = {
-        'title': new_title,
-        'body': new_body,
-        'labels': labels,
-        'milestone': milestone
+        'body': new_body
     }
-    response = requests.post(issue_url, headers=headers, json=data)
-    print(f"Response: {response}")
+    response = requests.patch(issue_url, headers=headers, json=data)
     if response.status_code == 200:
         try:
-            issues = response.json()
-            # Debug: print the type and content of the issues variable
-            print(f"Received data type: {type(issues)}")
-            print(f"Received data: {issues}")
-            return issues
+            updated_issue = response.json()
+            print(f"Issue updated: {updated_issue.get('title')}")
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
-            return []
-        return response.json()  # Parse JSON only if the response is OK
     else:
-        print(f"Error fetching issues: {response.status_code}")
+        print(f"Error updating issue: {response.status_code}")
         print(response.text)
-        return []  # Return an empty list on error
 
-    return response.json()
+# Load the updated requirements from the CSV file
+csv_file_path = 'github_requirements_updated_specific_req2.csv'
+requirements_df = pd.read_csv(csv_file_path)
+requirements_dict = requirements_df.set_index('Title')['Body'].to_dict()
 
-print(f"Starting the program")
+print("Starting the program")
 issues = list_issues()
 
 for issue in issues:
     title = issue['title']
     body = issue['body']
 
-    # Check if 'missile' is in the title or body
-    if 'missile' in title.lower() or 'missile' in body.lower():
-        new_title = title.replace('missile', 'T-shirt')
-        new_body = body.replace('missile', 'T-shirt')
-        update_issue(issue['number'], new_title, new_body, issue['labels'], issue['milestone'] )
+    # Check if the title is in the requirements dictionary
+    if title in requirements_dict:
+        new_body = requirements_dict[title]
+        update_issue(issue['number'], new_body)
