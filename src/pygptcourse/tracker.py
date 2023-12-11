f01d66a4982167720060de5c1562ab64d8df2a22
gshiva <mrchatgptbiz@gmail.com>
2023-12-10 16:57:58 -0800

    3/3 Apply Black formatting for code style consistency.
    Addresses #29
    
    Based on ChatGPT analysis the original commit that included
    black and mypy fixes was split into 3 commits. This is the last one.

--- src/pygptcourse/tracker.py
+++ src/pygptcourse/tracker.py
@@ -1,5 +1,5 @@
 import cv2
-import face_recognition # type: ignore
+import face_recognition  # type: ignore
 
 # Set the total time for moving from left to right and top to bottom
 TOTAL_TIME_LR = 26
@@ -29,6 +29,7 @@ video_capture.set(4, 480)  # Set vertical resolution
 counter = 0
 face_names = []
 
+
 def move_camera_to_center():
     global current_camera_position
     print("Moving camera to center")
@@ -41,19 +42,21 @@ def move_camera_to_center():
     move_camera("DOWN", TOTAL_TIME_TB)
 
     # set the current position to the known bottom left position
-    current_camera_position = [0,TOTAL_TIME_TB]
+    current_camera_position = [0, TOTAL_TIME_TB]
 
     # we are sure that the camera is on the bottom left
     # now move it to the center (13,2)
-    move_camera("RIGHT", TOTAL_TIME_LR/2)
-    move_camera("UP", TOTAL_TIME_TB/2)
+    move_camera("RIGHT", TOTAL_TIME_LR / 2)
+    move_camera("UP", TOTAL_TIME_TB / 2)
+
 
 counter = 0
+
 
 def detect_faces():
     global counter
     face_locations = []
-    face_encodings  = []
+    face_encodings = []
 
     ret, image = video_capture.read()
     print(f"Video captured")
@@ -71,7 +74,9 @@ def detect_faces():
 
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            match = face_recognition.compare_faces([shiva_face_encoding, adil_face_encoding], face_encoding)
+            match = face_recognition.compare_faces(
+                [shiva_face_encoding, adil_face_encoding], face_encoding
+            )
             name = "Unknown"
 
             if match[0]:
@@ -81,7 +86,7 @@ def detect_faces():
 
             face_names.append(name)
     print(f"Face names: {face_names}")
-    cv2.imshow('Video', image)
+    cv2.imshow("Video", image)
 
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Because we made the image smaller, now need to multiply by 4 to get correct size
@@ -91,15 +96,34 @@ def detect_faces():
         left *= 4
         cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        name = name + ": r:" + str(right) + ", b:" + str(bottom) + ", l:" + str(left) + ", t:" + str(top)
+        name = (
+            name
+            + ": r:"
+            + str(right)
+            + ", b:"
+            + str(bottom)
+            + ", l:"
+            + str(left)
+            + ", t:"
+            + str(top)
+        )
         print(f"Name: {name}")
 
         # Draw a label with a name below the face
-        cv2.rectangle(image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        cv2.putText(image, name, (left + 6, bottom - 6),  cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 1)
+        cv2.rectangle(
+            image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED
+        )
+        cv2.putText(
+            image,
+            name,
+            (left + 6, bottom - 6),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1.0,
+            (255, 255, 255),
+            1,
+        )
 
-
-        cv2.imshow('Video', image)
+        cv2.imshow("Video", image)
         counter += 1
 
         return [[top, right, bottom, left]]
@@ -141,13 +165,19 @@ def move_camera(direction, duration):
     current_camera_position[0] = max(0, min(current_camera_position[0], TOTAL_TIME_LR))
     current_camera_position[1] = max(0, min(current_camera_position[1], TOTAL_TIME_TB))
 
-    print(f"Previous position: {prev_current_camera_position} Calculated current position: {current_camera_position}, Direction: {direction}, Duration: {duration}")
+    print(
+        f"Previous position: {prev_current_camera_position} Calculated current position: {current_camera_position}, Direction: {direction}, Duration: {duration}"
+    )
 
     if prev_current_camera_position == current_camera_position:
         # nothing to do
-        print(f"Nothing to do. Current position: {current_camera_position} is same as previous position {prev_current_camera_position}.")
+        print(
+            f"Nothing to do. Current position: {current_camera_position} is same as previous position {prev_current_camera_position}."
+        )
         return
-    print(f"Moving to position: {current_camera_position}, Direction: {direction}, Duration: {duration}")
+    print(
+        f"Moving to position: {current_camera_position}, Direction: {direction}, Duration: {duration}"
+    )
     # TODO: issue usb move command
 
 
@@ -168,11 +198,14 @@ def track_face():
         # to get the x-coordinate of the center, and adding the top and bottom values and dividing by 2 to get the y-coordinate
         # of the center. The result is a list containing the x and y coordinates of the center of the bounding box.
 
-        face_center = [(face_bbox[1] + face_bbox[3]) // 2, (face_bbox[0] + face_bbox[2]) // 2]
+        face_center = [
+            (face_bbox[1] + face_bbox[3]) // 2,
+            (face_bbox[0] + face_bbox[2]) // 2,
+        ]
 
         # Calculate the distance from the face center to the image center
-        dx = face_center[0] - (IMAGE_WIDTH/2)
-        dy = face_center[1] - (IMAGE_HEIGHT/2)
+        dx = face_center[0] - (IMAGE_WIDTH / 2)
+        dy = face_center[1] - (IMAGE_HEIGHT / 2)
 
         # emprical calculations which came to 120 pixels both along the height and width
         # from the center of the image
@@ -182,11 +215,13 @@ def track_face():
         # CENTERX_MIN = round((IMAGE_WIDTH/2) - (IMAGE_WIDTH/(5*2)))
         # CENTERX_MAX = round((IMAGE_WIDTH/2) + (IMAGE_WIDTH/(4*2)))
 
-        TOLERANCE = (IMAGE_HEIGHT / (4*2)) # for 480 it is 60
+        TOLERANCE = IMAGE_HEIGHT / (4 * 2)  # for 480 it is 60
 
         # Move the camera in small steps in the x direction based on the distance from the face center
         while abs(dx) > TOLERANCE or abs(dy) > TOLERANCE:
-            print(f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}")
+            print(
+                f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}"
+            )
             if dx > TOLERANCE:
                 while dx > TOLERANCE:
                     move_camera("RIGHT", TIME_INCREMENT)
@@ -194,10 +229,15 @@ def track_face():
                     faces = detect_faces()
                     if faces and len(faces) > 0:
                         face_bbox = faces[0]
-                        face_center = [(face_bbox[1] + face_bbox[3]) // 2, (face_bbox[0] + face_bbox[2]) // 2]
-                        dx = face_center[0] - (IMAGE_WIDTH/2)
-                        dy = face_center[1] - (IMAGE_HEIGHT/2)
-                        print(f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}")
+                        face_center = [
+                            (face_bbox[1] + face_bbox[3]) // 2,
+                            (face_bbox[0] + face_bbox[2]) // 2,
+                        ]
+                        dx = face_center[0] - (IMAGE_WIDTH / 2)
+                        dy = face_center[1] - (IMAGE_HEIGHT / 2)
+                        print(
+                            f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}"
+                        )
 
             elif dx < -TOLERANCE:
                 while dx < -TOLERANCE:
@@ -207,13 +247,20 @@ def track_face():
                     faces = detect_faces()
                     if faces and len(faces) > 0:
                         face_bbox = faces[0]
-                        face_center = [(face_bbox[1] + face_bbox[3]) // 2, (face_bbox[0] + face_bbox[2]) // 2]
-                        dx = face_center[0] - (IMAGE_WIDTH/2)
-                        dy = face_center[1] - (IMAGE_HEIGHT/2)
-                        print(f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}")
+                        face_center = [
+                            (face_bbox[1] + face_bbox[3]) // 2,
+                            (face_bbox[0] + face_bbox[2]) // 2,
+                        ]
+                        dx = face_center[0] - (IMAGE_WIDTH / 2)
+                        dy = face_center[1] - (IMAGE_HEIGHT / 2)
+                        print(
+                            f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}"
+                        )
             # Move the camera in small steps in the y direction based on the distance from the face center
             if dy > TOLERANCE:
-                print(f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}")
+                print(
+                    f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}"
+                )
                 while dy > TOLERANCE:
                     move_camera("DOWN", TIME_INCREMENT)
 
@@ -221,12 +268,19 @@ def track_face():
                     faces = detect_faces()
                     if faces and len(faces) > 0:
                         face_bbox = faces[0]
-                        face_center = [(face_bbox[1] + face_bbox[3]) // 2, (face_bbox[0] + face_bbox[2]) // 2]
-                        dx = face_center[0] - (IMAGE_WIDTH/2)
-                        dy = face_center[1] - (IMAGE_HEIGHT/2)
-                        print(f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}")
+                        face_center = [
+                            (face_bbox[1] + face_bbox[3]) // 2,
+                            (face_bbox[0] + face_bbox[2]) // 2,
+                        ]
+                        dx = face_center[0] - (IMAGE_WIDTH / 2)
+                        dy = face_center[1] - (IMAGE_HEIGHT / 2)
+                        print(
+                            f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}"
+                        )
             if dy < -TOLERANCE:
-                print(f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}")
+                print(
+                    f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}"
+                )
                 while dy < -TOLERANCE:
                     move_camera("UP", TIME_INCREMENT)
 
@@ -234,10 +288,16 @@ def track_face():
                     faces = detect_faces()
                     if faces and len(faces) > 0:
                         face_bbox = faces[0]
-                        face_center = [(face_bbox[1] + face_bbox[3]) // 2, (face_bbox[0] + face_bbox[2]) // 2]
-                        dx = face_center[0] - (IMAGE_WIDTH/2)
-                        dy = face_center[1] - (IMAGE_HEIGHT/2)
-                        print(f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}")
+                        face_center = [
+                            (face_bbox[1] + face_bbox[3]) // 2,
+                            (face_bbox[0] + face_bbox[2]) // 2,
+                        ]
+                        dx = face_center[0] - (IMAGE_WIDTH / 2)
+                        dy = face_center[1] - (IMAGE_HEIGHT / 2)
+                        print(
+                            f"face center: {face_center} face_bbox = {face_bbox} dx: {dx}, dy: {dy}"
+                        )
+
 
 move_camera_to_center()
 while True:
@@ -246,7 +306,7 @@ while True:
 
     # Check if the 'q' key was pressed
     key = cv2.waitKey(1)
-    if key == ord('q'):
+    if key == ord("q"):
         break
 
 video_capture.release()
