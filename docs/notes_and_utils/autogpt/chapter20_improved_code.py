3feb188cb4c9006d4975aebcefbc9ee0dc171a61
gshiva <mrchatgptbiz@gmail.com>
2023-12-17 09:58:29 -0800

    pre-commit fixes for all project files
    Add Initial Test for pytest Hook Validation
    
    This commit adds a basic test to initiate the testing framework in our
    project. The test is a simple assertion to ensure the setup works correctly
    and the pytest pre-commit hook passes.
    
    - Added `test_basic.py` with a simple passing test.
    
    This is the first step towards building a comprehensive test suite for
    the project.
    
    Addresses #38
    Addresses #29

--- docs/notes_and_utils/autogpt/chapter20_improved_code.py
+++ docs/notes_and_utils/autogpt/chapter20_improved_code.py
@@ -2,6 +2,6 @@ def fibonacci(n):
     if n <= 1:
         return n
     a, b = 0, 1
-    for i in range(2, n+1):
-        a, b = b, a+b
+    for i in range(2, n + 1):
+        a, b = b, a + b
     return b
