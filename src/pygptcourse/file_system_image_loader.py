8b7e9453f6131158fa677489040f9f99e4e5b835
gshiva <gshiva@hotmail.com>
2023-12-27 13:06:28 -0800

    bugfix: Use the script location for the faces not cwd

--- src/pygptcourse/file_system_image_loader.py
+++ src/pygptcourse/file_system_image_loader.py
@@ -8,7 +8,7 @@ class ImageLoader:
 
 class FileSystemImageLoader(ImageLoader):
     def __init__(self, base_dir=None):
-        self.base_dir = base_dir or os.getcwd()
+        self.base_dir = base_dir or os.path.dirname(os.path.abspath(__file__))
 
     def get_full_image_path(self, image_name):
         full_image_path = os.path.join(self.base_dir, image_name)
